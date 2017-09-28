b98df00306460cbd74f6cd9224139f5a77e84192
pana pangpanpat <panapangpanpat@fico.com>
2017-09-28 10:49:17 +0700

    add createPromotion api

--- src/actions/Api.js
+++ src/actions/Api.js
@@ -16,6 +16,23 @@ function getPromotions(cb) {
         .then(cb);
 }
 
+function createPromotion(promotion) {
+    console.log("in api createPromotion");
+    const headers = { 'Content-Type': 'application/json' };
+    const request = new Request('/promotions', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ promotion: promotion })
+    });
+
+
+    return fetch(request).then(response => {
+        return response.json();
+    }).catch(error => {
+        return error;
+    });
+}
+
 function checkStatus(response) {
     if (response.status >= 200 && response.status < 300) {
         return response;
@@ -31,5 +48,5 @@ function parseJSON(response) {
     return response.json();
 }
 
-const Api = { getRestaurant, getPromotions };
+const Api = { getRestaurant, getPromotions, createPromotion };
 export default Api;
